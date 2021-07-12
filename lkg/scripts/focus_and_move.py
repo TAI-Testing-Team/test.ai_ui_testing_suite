f0c3416ae7679041e594a3c99a412521d696715c
Alex Chappo <alexchappo@gmail.com>
2021-07-12 09:53:57 -0400

    Tests made through 17. Many tests reworked.Tests pushed up through July 12th on this branch.

--- lkg/scripts/focus_and_move.py
+++ lkg/scripts/focus_and_move.py
@@ -6,5 +6,5 @@ log = logging.getLogger(__name__)
 def run(context):
 
     context.perform_gesture('tap', 'inp_description')
-    context.perform_gesture('swipe_down','')
+    context.perform_gesture('swipe_down', '')
     context.get_all_elements()
